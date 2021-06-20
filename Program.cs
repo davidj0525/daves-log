87ccc7b4a1a80f198fb07573bbc08b2a092fcc92
David Jennings <davidj0525@yahoo.com>
2021-06-20 16:48:06 -0700

    Hello World

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace daves_log
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
