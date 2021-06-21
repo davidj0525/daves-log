7faaeac95fea550b1db93f784ad9bb9e531f5e11
Justin Joseph <jmjoseph20@hotmail.com>
2021-06-20 20:00:35 -0400

    I said Hello Dave

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace daves_log
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Dave!");
         }
     }
 }
