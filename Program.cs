f54be270a0bdcdd87f93415ca8e41acf454e495c
Justin Joseph <jmjoseph20@hotmail.com>
2021-06-20 20:18:47 -0400

    Changed background color

--- Program.cs
+++ Program.cs
@@ -9,6 +9,10 @@ namespace daves_log
             Console.WriteLine("Hello Dave!");
 
             Console.WriteLine("This is ground control to Major Tom.");
+
+            Console.WriteLine("Printer is always out of????");
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
         }
     }
 }
